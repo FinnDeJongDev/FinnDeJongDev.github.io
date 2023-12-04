206b65a1dec1c3261bd35b2acae2b8aeeea0bfcd
FinnDeJongDev <finn.dejong.dev@gmail.com>
2023-12-04 16:03:20 +0100

    link update

--- src/config/links.config.js
+++ src/config/links.config.js
@@ -2,7 +2,7 @@ const links = {
   github: 'https://github.com/FinnDeJongDev',
   linkedIn: 'https://www.linkedin.com/in/finn-de-jong-73783a19b/',
   bestProject: 'https://www.bluecurrent.nl/',
-  lastProject: 'https://finndejongdev.github.io/portfolio-2023/',
+  lastProject: 'https://finndejong.dev/',
 }
 
 export default links
