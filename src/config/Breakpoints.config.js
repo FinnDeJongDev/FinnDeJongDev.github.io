2de393869e9566ad5dbb4a73780e00e5f2755de6
FinnDeJongDev <finn.dejong.dev@gmail.com>
2024-12-16 14:00:11 +0100

    2025 porfolio first iteration

--- src/config/Breakpoints.config.js
+++
@@ -1,8 +0,0 @@
-const Breakpoints = {
-  desktop: {
-    width: 1280,
-    height: 720,
-  },
-}
-
-export default Breakpoints
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000
-  },
-  base: "/",
   plugins: [react()],
 })
